076fbb204398b95571937ce121985a6f507cec42
chetanramesh97 <30743260+chetanramesh97@users.noreply.github.com>
2025-07-08 07:03:59 +0530

    Update gcp_pyspark_schedule.sh

--- gcp_pyspark_schedule.sh
+++ gcp_pyspark_schedule.sh
@@ -1,13 +1,10 @@
-gcloud dataproc jobs submit pyspark \
-  --cluster=cluster-de-usecase \
-  --region=us-central1 \
-  --properties="spark.driver.memory=2g,spark.executor.memory=2g,spark.executor.instances=4,spark.executor.cores=2,spark.submit.deployMode=client,spark.sql.shuffle.partitions=10,spark.shuffle.spill.compress=true" \
-  /home/chetandeprac/project/Usecase4_GcpGcsReadWritehive_cloud.py
-
-if [ $? -ne 0 ]; then
-  echo "$(date) - Error occurred in the PySpark job" > /tmp/gcp_pyspark_schedule.log
+gcloud dataproc jobs submit pyspark --cluster=cluster-de-usecase --region=us-central1 --properties="spark.driver.memory=2g","spark.executor.memory=2g","spark.executor.instances=4","spark.executor.cores=2","spark.submit.deployMode=client","spark.sql.shuffle.partitions=10","spark.shuffle.spill.compress=true" /home/chetandeprac/project/Usecase4_GcpGcsReadWritehive_cloud.py
+#Alternative for spark-submit inside the dataproc cluster
+#spark-submit --master yarn --mode client --num-executors 4….
+if [ $? -ne 0 ]
+then
+echo "'date' error occured in the Pyspark job" > /tmp/gcp_pyspark_schedule.log
 else
-  echo "$(date) - PySpark job completed successfully" > /tmp/gcp_pyspark_schedule.log
+echo "'date' Pyspark job is completed successfully" > /tmp/gcp_pyspark_schedule.log
 fi
-
-echo "$(date) - gcloud PySpark ETL script completed" >> /tmp/gcp_pyspark_schedule.log
+echo "'date' gcloud pyspark ETL script is completed" >> /tmp/gcp_pyspark_schedule.log
